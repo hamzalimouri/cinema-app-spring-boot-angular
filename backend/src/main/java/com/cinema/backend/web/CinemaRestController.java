1fcc7c434401b45461c6af19908493b67d368e63
Hamza LIMOURI <limouri.hamza@gmail.com>
2021-05-17 09:22:32 +0100

    Added endpoint /payerTickets to paying tickets.

--- backend/src/main/java/com/cinema/backend/web/CinemaRestController.java
+++ backend/src/main/java/com/cinema/backend/web/CinemaRestController.java
@@ -1,25 +1,36 @@
 package com.cinema.backend.web;
 
 import java.io.File;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.transaction.Transactional;
 
 import com.cinema.backend.dao.FilmRepository;
+import com.cinema.backend.dao.TicketRepository;
 import com.cinema.backend.entities.Film;
+import com.cinema.backend.entities.Ticket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
 
 @RestController
 public class CinemaRestController {
     @Autowired
     private FilmRepository filmRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
 
     @GetMapping(path = "/imageFilm/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     @Transactional
@@ -31,4 +42,24 @@ public class CinemaRestController {
         return Files.readAllBytes(path);
 
     }
+
+    @PostMapping("/payerTickets")
+    @Transactional
+    public List<Ticket> payerTickets(@RequestBody TicketForm ticketForm) {
+        List<Ticket> listTickets = new ArrayList<Ticket>();
+        ticketForm.getTickets().forEach(idTicket -> {
+            Ticket ticket = ticketRepository.findById(idTicket).get();
+            ticket.setNomClient(ticketForm.getNameClient());
+            ticket.setReserve(true);
+            ticketRepository.save(ticket);
+            listTickets.add(ticket);
+        });
+        return listTickets;
+    }
+}
+
+@Data
+class TicketForm implements Serializable {
+    private String nameClient;
+    private List<Long> tickets = new ArrayList<Long>();
 }
