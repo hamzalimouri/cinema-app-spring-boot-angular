f0ed0635e39c541eb57da57851452472936cf344
Hamza LIMOURI <limouri.hamza@gmail.com>
2021-05-17 02:43:55 +0100

    Fixed Ticket and Seance Entities.

--- backend/src/main/java/com/cinema/backend/entities/Seance.java
+++ backend/src/main/java/com/cinema/backend/entities/Seance.java
@@ -21,6 +21,6 @@ public class Seance {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.TIME)
     private Date heureDebut;
 }
